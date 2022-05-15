385b693aadcbc6705ef4a7533c98724dbd1c07a9
kongsoee <thdl226@naver.com>
2022-05-15 22:51:23 +0900

    final

--- src/component/Box.js
+++ src/component/Box.js
@@ -18,7 +18,7 @@ const Box = (props) => {
     <div className={`box ${result}`}>
         <h1>{props.title}</h1>
       
-        <img className='item-img' src={props.item && props.item.img} alt="v"/>
+        <img className='item-img' src={props.item && props.item.img}/>
         <h2>{result}</h2>
     </div>
   )
